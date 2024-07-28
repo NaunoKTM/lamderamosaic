4040c4632cf62e342bdc7c91252923c0037e5a09
NaunoKTM <arnaud.c31@icloud.com>
2024-07-28 23:07:26 +0700

    Preparing for v2

---
+++ src/Evergreen/V2/Mosaic.elm
@@ -0,0 +1,50 @@
+module Evergreen.V2.Mosaic exposing (..)
+
+
+type alias PictureSize =
+    { width : Int
+    , height : Int
+    }
+
+
+type alias Picture =
+    { id : String
+    , size : PictureSize
+    }
+
+
+type Modal
+    = PictureOpen Int
+    | ModalClosed
+
+
+type alias DisplayConfig =
+    { baseWidth : Int
+    , baseHeight : Int
+    , spacingSize : Int
+    }
+
+
+type alias Mosaic =
+    { pictures : List Picture
+    , modal : Modal
+    , config : DisplayConfig
+    , screenSize :
+        { deviceWidth : Int
+        , deviceHeight : Int
+        }
+    }
+
+
+type KeyBoardKey
+    = Left
+    | Right
+    | Escape
+    | Other
+
+
+type Msg
+    = NoOpMsg
+    | ModalOpen Int
+    | ModalExit
+    | ReceiveKeyboardEvent KeyBoardKey
